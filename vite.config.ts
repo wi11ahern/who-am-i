8b47c9254f9a269baf51b5b55a5a8bc5fbe8e09a
William Ahern <willdahern@gmail.com>
2023-03-13 21:27:14 -0400

    Route web traffic through nginx

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: "0.0.0.0",
-    port: 3000,
-    strictPort: true,
-  },
 });
