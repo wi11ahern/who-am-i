f23607e6cab07979117253180f7dded14124a017
William Ahern <willdahern@gmail.com>
2023-03-16 21:16:43 -0400

    Add placeholder body content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Header from "./components/layout/header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <h3 style={{ textAlign: "center", padding: "5rem 0 0 0" }}>
+        Can you believe there's no content here?
+      </h3>
+      ;<h3 style={{ textAlign: "center", padding: "1rem 0 0 0" }}>The audacity!</h3>
+    </>
+  );
 }
 
 export default App;
