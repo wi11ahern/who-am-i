18be497ab11047d32b34ba5b604c79fa9f8efca6
William Ahern <willdahern@gmail.com>
2023-03-19 15:59:55 -0400

    Fix header styling

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ function App() {
       <h3 style={{ textAlign: "center", padding: "5rem 0 0 0" }}>
         Can you believe there's no content here?
       </h3>
-      ;<h3 style={{ textAlign: "center", padding: "1rem 0 0 0" }}>The audacity!</h3>
+      ;
+      <h3 style={{ textAlign: "center", padding: "1rem 0 0 0" }}>
+        The audacity!
+      </h3>
     </>
   );
 }
--- src/components/layout/whoami-logo.tsx
+++ src/components/layout/whoami-logo.tsx
@@ -1,15 +1,21 @@
 import styles from "./whoami-logo.module.css";
 
 const WhoAmILogo = () => {
+  const reloadHandler = () => {
+    location.reload();
+  };
+
   return (
-    <div className={styles.terminal}>
-      <p className={styles.command}>
-        <span>$</span>whoami
-      </p>
-      <p className={styles.stdout}>
-        <span>william</span>
-      </p>
-    </div>
+    <button className={styles.btn} onClick={reloadHandler}>
+      <div className={styles.terminal}>
+        <p className={styles.command}>
+          <span>$</span>whoami
+        </p>
+        <p className={styles.stdout}>
+          <span>william</span>
+        </p>
+      </div>
+    </button>
   );
 };
 
