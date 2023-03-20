43889cc18c337df0dd8f7d01586ba107bd412208
William Ahern <willdahern@gmail.com>
2023-03-20 12:56:13 -0400

    Change Hey there to welcome

--- src/components/greeting/greeting.tsx
+++ src/components/greeting/greeting.tsx
@@ -6,13 +6,13 @@ interface Props {}
 const Greeting = (props: Props) => {
   return (
     <Card className={styles.greeting}>
-      <p className={styles["greeting-top"]}>Hey there! My name is</p>
+      <p className={styles["greeting-top"]}>Welcome! My name is</p>
       <h1 className={styles.name}>
         <span className={styles.bracket}>&gt;</span>William Ahern.
       </h1>
       <p>
         I am a software engineer with experience across the stack, but perhaps
-        more importantly
+        more importantly,
       </p>
       <p className={styles["greeting-bot"]}>
         I am a <span>builder</span>, <span>problem solver</span>, and{" "}
