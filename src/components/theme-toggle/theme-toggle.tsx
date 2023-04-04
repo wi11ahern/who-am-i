f70284e1d06a032a547bca93d74ad9eefaeba622
Will Ahern <willdahern@gmail.com>
2023-04-04 08:09:55 -0400

    Start in dark mode

--- src/components/theme-toggle/theme-toggle.tsx
+++ src/components/theme-toggle/theme-toggle.tsx
@@ -4,14 +4,18 @@ import { RootState } from "../../store/store";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMoon, faLightbulb } from "@fortawesome/free-solid-svg-icons";
 import styles from "./theme-toggle.module.css";
+import { useRef, useState } from "react";
 
 interface Props {}
 
 const ThemeToggle = () => {
+  const toggleRef = useRef<HTMLInputElement>(null);
+  const [checked, setChecked] = useState(true);
   const dispatcher = useDispatch();
   const theme = useSelector((state: RootState) => state.ui.theme);
 
   const onToggleHandler = () => {
+    setChecked((prevState) => !prevState);
     dispatcher(
       uiActions.setTheme(theme === Theme.Light ? Theme.Dark : Theme.Light)
     );
@@ -19,7 +23,13 @@ const ThemeToggle = () => {
 
   return (
     <label htmlFor="theme-toggle" className={styles.switch}>
-      <input id="theme-toggle" type="checkbox" onChange={onToggleHandler} />
+      <input
+        id="theme-toggle"
+        type="checkbox"
+        onChange={onToggleHandler}
+        ref={toggleRef}
+        checked={checked}
+      />
       <span className={styles.slider}></span>
       <FontAwesomeIcon icon={faMoon} className={styles.moon} />
       <FontAwesomeIcon icon={faLightbulb} className={styles.lightbulb} />
