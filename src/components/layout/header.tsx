8fa822d2ab3fb0eed6a0dc39c6fe64b52c574ccc
Will Ahern <willdahern@gmail.com>
2023-04-15 08:58:02 -0400

    style: update header to use tailwind css

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,17 +1,18 @@
-import { useSelector } from "react-redux";
 import useScrollDirection from "../../hooks/useScrollDirection";
-import { RootState } from "../../store/store";
 import ThemeToggle from "../theme-toggle/theme-toggle";
 import MainNav from "./main-nav";
 import WhoAmILogo from "./whoami-logo";
 
 const Header = () => {
   const scrollDirection = useScrollDirection();
-  // const headerScrollClass = scrollDirection === "down" ? styles["hide"] : "";
-  const theme = useSelector((state: RootState) => state.ui.theme);
+  const headerHeight = scrollDirection === "down" ? "h-0 border-none" : "h-headerHeight border-b";
 
   return (
-    <header className="w-full h-headerHeight">
+    <header
+      className={`flex w-full ${headerHeight} top-0 z-10 fixed content-center bg-slate-800 shadow-lg 
+                  transition-all ease-in-out duration-500 overflow-hidden
+                  dark:bg-slate-950 border-gray-300`}
+    >
       <WhoAmILogo />
       <ThemeToggle />
       <MainNav />
