077de94f2c7871804350410cd40121b761f528da
Will Ahern <willdahern@gmail.com>
2023-04-17 09:10:51 -0400

    style: update greeting component to use tailwind css

--- src/components/greeting/greeting.tsx
+++ src/components/greeting/greeting.tsx
@@ -1,27 +1,20 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../store/store";
 import Card from "../ui/card";
 import styles from "./greeting.module.css";
 
-interface Props {}
-
-const Greeting = (props: Props) => {
-  const theme = useSelector((state: RootState) => state.ui.theme);
-
+const Greeting = () => {
   return (
-    <Card className={styles.greeting}>
-      <p className={styles["greeting-top"]}>Welcome! My name is</p>
-      <h1 className={`${styles.name} ${styles[theme]}`}>
-        {/* <span className={`${styles.bracket} ${styles[theme]}`}>&gt;</span> */}
+    <Card className="mx-auto px-8 h-screen">
+      <p className="text-2xl">Hi, my name is</p>
+      <h1 className="text-8xl">
         William Ahern<span className={styles.period}>.</span>
       </h1>
-      <p className={`${styles["greeting-bot"]} ${styles[theme]}`}>
-        I am a software engineer with experience across the stack, but perhaps
-        more importantly,
+      <p className="text-2xl">
+        I am a software engineer with experience across the stack, but more importantly,
       </p>
-      <p className={`${styles["greeting-bot"]} ${styles[theme]}`}>
-        I am a <span>builder</span>, <span>problem solver</span>, and{" "}
-        <span>lifelong learner</span>.
+      <p className="text-2xl">
+        I am a <span className="text-yellow-500">builder</span>,{" "}
+        <span className="text-yellow-500">problem solver</span>, and{" "}
+        <span className="text-yellow-500">lifelong learner</span>.
       </p>
     </Card>
   );
