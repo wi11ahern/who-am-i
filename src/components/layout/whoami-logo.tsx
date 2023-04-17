5e8fa9f0f5d953ac9c1dbaa6e64a8b8f44064b97
Will Ahern <willdahern@gmail.com>
2023-04-17 07:48:34 -0400

    style: increase logo font size

--- src/components/layout/whoami-logo.tsx
+++ src/components/layout/whoami-logo.tsx
@@ -10,7 +10,7 @@ const WhoAmILogo = () => {
       className="bg-transparent text-left border-solid border-r border-gray-300 h-full w-30 p-3"
       onClick={backToTopHandler}
     >
-      <p>
+      <p className="text-lg">
         <span className="text-yellow-500 pr-2">$</span>whoami
       </p>
       <span className="text-xl text-yellow-500">william</span>
