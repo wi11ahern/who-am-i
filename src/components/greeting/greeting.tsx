7f78baf56299518a0e7a342d65ddc75b3cd9f32c
William Ahern <willdahern@gmail.com>
2023-04-17 22:16:05 -0400

    styles: update greeting text colors

--- src/components/greeting/greeting.tsx
+++ src/components/greeting/greeting.tsx
@@ -1,20 +1,20 @@
 import Card from "../ui/card";
-import styles from "./greeting.module.css";
 
 const Greeting = () => {
   return (
     <Card className="mx-auto px-8 h-screen">
       <p className="text-2xl">Hi, my name is</p>
-      <h1 className="text-8xl">
-        William Ahern<span className={styles.period}>.</span>
+      <h1 className="text-8xl text-yellow-500">
+        William Ahern<span className="text-white">.</span>
       </h1>
       <p className="text-2xl">
-        I am a software engineer with experience across the stack, but more importantly,
+        I am a software engineer with experience across the stack, but more
+        importantly,
       </p>
       <p className="text-2xl">
-        I am a <span className="text-yellow-500">builder</span>,{" "}
-        <span className="text-yellow-500">problem solver</span>, and{" "}
-        <span className="text-yellow-500">lifelong learner</span>.
+        I am a <span className="text-emerald-500">builder</span>,{" "}
+        <span className="text-emerald-500">problem solver</span>, and{" "}
+        <span className="text-emerald-500">lifelong learner</span>.
       </p>
     </Card>
   );
