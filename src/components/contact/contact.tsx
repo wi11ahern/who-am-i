f2f6afb11b9bf77ecf14ba48fcf53470d9f7d901
William Ahern <willdahern@gmail.com>
2023-04-17 22:05:49 -0400

    style: update contact component to use tailwind css, add wiggle animation

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -7,19 +7,27 @@ interface Props {}
 
 const Contact = (props: Props) => {
   return (
-    <Card className={styles.contact} id="contact">
-      <h2>03. Contact</h2>
+    <Card
+      className="flex items-center flex-col min-w-[600px] max-w-[600px] mx-auto"
+      id="contact"
+    >
+      <h2 className="text-xl font-bold underline underline-offset-4 pb-4 text-yellow-500">
+        03. Contact
+      </h2>
       <p>
         Have a question? Just want to say hi? No problem! My inbox is always
         open.
       </p>
       <a
-        className={styles["email-link"]}
+        className="mx-auto mt-4"
         href="mailto:williamahern.com@gmail.com"
         rel="noopener noreferrer"
         target="_blank"
       >
-        <FontAwesomeIcon icon={faEnvelope} className={styles["email-icon"]} />
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          className="w-[50px] h-[50px] text-blue-400 hover:text-emerald-500 hover:animate-wiggle"
+        />
       </a>
     </Card>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ export default {
       height: {
         headerHeight: "5.5rem",
       },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-5deg)" },
+          "50%": { transform: "rotate(5deg)" },
+        },
+      },
+      animation: {
+        wiggle: "wiggle .5s ease-in-out infinite",
+      },
     },
   },
   darkMode: "class",
