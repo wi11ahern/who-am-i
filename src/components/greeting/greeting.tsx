9f5471ac286cd9b329a9050fb1c3dd254ae3d83b
William Ahern <willdahern@gmail.com>
2023-04-19 15:13:51 -0400

    style: dynamically adjust greeting component height based on viewport

--- src/components/greeting/greeting.tsx
+++ src/components/greeting/greeting.tsx
@@ -2,7 +2,7 @@ import Card from "../ui/card";
 
 const Greeting = () => {
   return (
-    <Card className="mx-auto px-8 min-h-[calc(100vh_-_400px)] duration-[1.5s]">
+    <Card className="mx-auto px-8 mt-[40vh] py-0 min-h-[60vh] duration-[1.5s]">
       <p className="text-2xl">Hi, my name is</p>
       <h1 className="text-8xl text-yellow-500">
         William Ahern<span className="text-white">.</span>
