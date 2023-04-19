a4a8efdfe3fd88ed18f7e74ade3027d363798ba5
William Ahern <willdahern@gmail.com>
2023-04-18 20:22:02 -0400

    style: update main background

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div
       className="flex items-center justify-center w-full h-headerHeight content-center bg-slate-800 
-                  dark:bg-slate-950 border-solid border-t-[1px] border-gray-300 "
+                  dark:bg-slate-950 border-solid border-t-[1px] border-gray-300"
     >
       <p className="text-gray-400 text-sm">Designed & Built by William Ahern</p>
     </div>
