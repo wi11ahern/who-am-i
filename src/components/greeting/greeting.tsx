47747b5821750bb7bed8bb446717a7cd372769d2
Will Ahern <willdahern@gmail.com>
2023-04-24 11:30:53 -0400

    style: remove header component margin and use flexbox for alignment

--- src/components/greeting/greeting.tsx
+++ src/components/greeting/greeting.tsx
@@ -2,7 +2,7 @@ import Card from "../ui/card";
 
 const Greeting = () => {
   return (
-    <Card className="col-start-2 col-span-1 justify-self-center flex flex-col mx-0 mt-[40vh] py-0 min-h-[60vh] min-w-[200px] max-w-[900px] duration-[1.5s]">
+    <Card className="row-start-1 row-span-1 col-start-2 col-span-1 justify-self-center flex flex-col justify-center m-0 min-h-[100vh] min-w-[200px] max-w-[900px] duration-[1.5s]">
       <p className="text-2xl">Hi, my name is</p>
       <h1 className="text-yellow-500 text-8xl ">
         William Ahern<span className="text-white">.</span>
